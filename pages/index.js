bea6844f2ea095ceb86656a4617229815705c05e
electro9666 <electro9666@gmail.com>
2021-06-17 11:04:40 +0900

    add ignore

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-console.log(1);
+console.log(12);
 
 export default function Home() {
   return (
